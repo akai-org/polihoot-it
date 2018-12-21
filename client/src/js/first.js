fe4e428b0119a66de7e6f86e2ef8f8be212e3930
Adam Tomczyk <tomczyk006@gmail.com>
2018-12-21 21:29:25 +0100

    first commit

--- client/src/js/first.js
+++ client/src/js/first.js
@@ -2,10 +2,21 @@ import { container, socket } from './index';
 import { genSecondView } from './second';
 
 export const genFirstView = () => {
+  const h1 = document.createElement('h1');
+  h1.innerHTML = 'Polihood-it';
+  container.appendChild(h1);
+
+  const text = document.createElement('div');
+  text.setAttribute('id', text);
+  container.appendChild(text);
+
   const input = document.createElement('input');
-  container.appendChild(input);
+  input.setAttribute('type', 'text');
+  input.setAttribute('placeholder', 'nick');
+  text.appendChild(input);
+
   const button = document.createElement('button');
-  button.innerHTML = 'connect';
+  button.innerHTML = 'ENTER';
   container.appendChild(button);
 
   // join room
--- client/src/js/second.js
+++ client/src/js/second.js
@@ -1,25 +1,47 @@
 import { container, socket } from './index';
 
-export const genSecondView = roomName => {
-  const p = document.createElement('p');
-  p.innerHTML = `connected to ${roomName}`;
-  container.appendChild(p);
-  const input = document.createElement('input');
-  container.appendChild(input);
-  const button = document.createElement('button');
-  button.innerHTML = 'send message';
-  container.appendChild(button);
-  const messageBox = document.createElement('div');
-  container.appendChild(messageBox);
+export const genSecondView = nick => {
+  const h1 = document.createElement('h1');
+  h1.innerHTML = `Hey ${nick}`;
+  container.appendChild(h1);
 
+  const text = document.createElement('div');
+  text.setAttribute('id', 'text');
+  container.appendChild(text);
+
+  const input = document.createElement('input');
+  input.setAttribute('type', 'text');
+  input.setAttribute('style', "width: 70%; max-width: 500px;");
+  input.setAttribute('placeholder', 'create new room')
+  text.appendChild(input);
+  
+  const br = document.createElement('br');
+  text.appendChild(br);
+  
+  const style = document.createElement('div');
+  style.setAttribute('style', 'margin-top: 4em; color: #ccc;');
+  style.innerHTML = 'or choose one';
+  text.appendChild(style);
+
+  const button = document.createElement('button');
+  button.innerHTML = 'JOIN';
+  container.appendChild(button);
+
+  const messageBox = document.createElement('div');
+  // do poprawy!
+  var height = -100 + window.innerHeight - document.getElementById('text').offsetHeight;
+  messageBox.style.height = height;
+  messageBox.style.marginTop = document.getElementById('text').offsetHeight;
+  container.appendChild(messageBox);
+ 
   // sending message
   button.addEventListener('click', () => {
-    socket.emit('message', { message: input.value, room: roomName });
+    socket.emit('message', { message: input.value, room: nick });
   });
 
   // getting message
   socket.on('message', data => {
-    const newMessage = document.createElement('p');
+    const newMessage = document.createElement('li');
     console.log(data);
     newMessage.innerHTML = data.message;
     container.appendChild(newMessage);
